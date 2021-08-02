8f9b3dd8722da0197d9bf3682a02097f8b21ea81
Guilherme Marcionilo <guilhermemarcionilo@gmail.com>
2021-08-02 20:29:13 -0300

    Adicionando comentários

--- src/main/kotlin/br/com/zup/core/service/ToyServiceImpl.kt
+++ src/main/kotlin/br/com/zup/core/service/ToyServiceImpl.kt
@@ -7,6 +7,7 @@ import br.com.zup.core.port.ToyServicePort
 import br.com.zup.entrypoint.dto.ToyDto
 import br.com.zup.infrastructure.enum.TakeAction.*
 import br.com.zup.infrastructure.model.ToyEvent
+import org.slf4j.LoggerFactory
 import java.math.BigDecimal
 import java.util.*
 import javax.inject.Singleton
@@ -14,15 +15,19 @@ import javax.inject.Singleton
 @Singleton
 class ToyServiceImpl(private val toyInfrastructure: ToyInfrastructurePort): ToyServicePort {
 
+    private val LOG = LoggerFactory.getLogger(this::class.java)
+
     override fun create(toy: Toy) = toyInfrastructure.sendToy(ToyConverter.toyToToyEvent(toy, REGISTER))
 
     override fun update(id: UUID, toy: Toy) {
+        LOG.info("Enviando Toy para ser atualizado - Core | Service \n\n $toy, $id")
         val toyEvent = ToyConverter.toyToToyEvent(toy,UPDATE)
         toyEvent.toy.id = id
         toyInfrastructure.sendToy(toyEvent)
     }
 
     override fun delete(id: UUID) {
+        LOG.info("Enviando Toy para ser deletado - Core | Service \n\n $id")
         val toyEvent = ToyEvent(ToyDto(id, "?", BigDecimal.ONE, "?"), DELETE)
         toyInfrastructure.sendToy(toyEvent)
     }
--- src/main/kotlin/br/com/zup/entrypoint/controller/ToyController.kt
+++ src/main/kotlin/br/com/zup/entrypoint/controller/ToyController.kt
@@ -11,6 +11,7 @@ import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Put
 import io.micronaut.validation.Validated
+import org.slf4j.LoggerFactory
 import java.util.*
 import javax.validation.Valid
 
@@ -18,17 +19,21 @@ import javax.validation.Valid
 @Controller("/toy")
 class ToyController(private val toyServicePort: ToyServicePort) {
 
+    private val LOG = LoggerFactory.getLogger(this::class.java)
+
     @Post
     fun create(@Body @Valid dto: ToyDto) = HttpResponse.created(toyServicePort.create(ToyConverter.toToy(dto)))
 
     @Put("/{id}")
     fun update(@PathVariable id: UUID, @Valid @Body dto: ToyDto): HttpResponse<Unit> {
+        LOG.info("Possível Toy a ser Atualizado: $id")
         toyServicePort.update(id, ToyConverter.toToy(dto))
         return HttpResponse.noContent()
     }
 
     @Delete("/{id}")
     fun delete(@PathVariable id: UUID): HttpResponse<Unit> {
+        LOG.info("Possível Toy a ser Deletado: $id")
         toyServicePort.delete(id)
         return HttpResponse.noContent()
     }
